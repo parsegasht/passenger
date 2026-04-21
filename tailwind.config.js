7336ac07f3f743d5a76229706e4f07a2767c7718
parsegasht <Azimi.ahmad@outlook.com>
2026-04-21 10:33:21 +0330

    font CDNs replaced with google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'vazir': ['Vazirmatn', 'sans-serif'],
-        'iran': ['IRANSans', 'sans-serif'],
+        vazir: ["Vazirmatn", "sans-serif"],
+        iran: ["IRANSans", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-
+};
